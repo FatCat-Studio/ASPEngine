cb50c868c9163b02e42b1d4da1ac06b1ed7378ae
ASPCartman <aspcartman@gmail.com>
2012-04-24 20:31:50 +0400

    Initial Commit

---
+++ ASPEngine/ASPEngine.h
@@ -0,0 +1,13 @@
+//
+//  ASPEngine.h
+//  ASPEngine
+//
+//  Created by Руслан Федоров on 4/24/12.
+//  Copyright (c) 2012 MIPT iLab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ASPEngine : NSObject
+
+@end
---
+++ ASPEngine/ASPEngine.m
@@ -0,0 +1,13 @@
+//
+//  ASPEngine.m
+//  ASPEngine
+//
+//  Created by Руслан Федоров on 4/24/12.
+//  Copyright (c) 2012 MIPT iLab. All rights reserved.
+//
+
+#import "ASPEngine.h"
+
+@implementation ASPEngine
+
+@end
